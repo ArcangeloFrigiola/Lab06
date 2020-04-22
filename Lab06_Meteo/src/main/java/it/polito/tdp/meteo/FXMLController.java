5e46a994534c57fd5ad36a3333750965cbe32265
arcan <arcan@DESKTOP-84JU088.homenet.telecomitalia.it>
2020-04-22 12:34:39 +0200

    Laboratorio 6 finito e funzionante

--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/FXMLController.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/FXMLController.java
@@ -5,15 +5,26 @@
 package it.polito.tdp.meteo;
 
 import java.net.URL;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.meteo.model.Model;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
+import javafx.util.Callback;
 
 public class FXMLController {
-
+	
+	Model model;
+	ObservableList<Month> listaMesi = FXCollections.observableArrayList(getMesi());
+	
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
@@ -21,7 +32,7 @@ public class FXMLController {
     private URL location;
 
     @FXML // fx:id="boxMese"
-    private ChoiceBox<?> boxMese; // Value injected by FXMLLoader
+    private ChoiceBox<Month> boxMese; // Value injected by FXMLLoader
 
     @FXML // fx:id="btnUmidita"
     private Button btnUmidita; // Value injected by FXMLLoader
@@ -34,12 +45,36 @@ public class FXMLController {
 
     @FXML
     void doCalcolaSequenza(ActionEvent event) {
-
+    	
+    	this.txtResult.clear();
+    	Month mese =  this.boxMese.getValue();
+    	this.txtResult.appendText(this.model.calcolaSequenza(mese));
     }
 
-    @FXML
-    void doCalcolaUmidita(ActionEvent event) {
 
+	private List<Month> getMesi() {
+		List<Month> mesi = new ArrayList<>();
+		mesi.add(Month.JANUARY);
+		mesi.add(Month.FEBRUARY);
+		mesi.add(Month.MARCH);
+		mesi.add(Month.APRIL);
+		mesi.add(Month.MAY);
+		mesi.add(Month.JUNE);
+		mesi.add(Month.JULY);
+		mesi.add(Month.AUGUST);
+		mesi.add(Month.SEPTEMBER);
+		mesi.add(Month.OCTOBER);
+		mesi.add(Month.NOVEMBER);
+		mesi.add(Month.DECEMBER);
+		return mesi;
+	}
+
+
+	@FXML
+    void doCalcolaUmidita(ActionEvent event) {
+    	this.txtResult.clear();
+		int mese = this.listaMesi.indexOf(this.boxMese.getValue())+1;
+		this.txtResult.appendText(this.model.getUmiditaMedia(mese));
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -49,6 +84,9 @@ public class FXMLController {
         assert btnCalcola != null : "fx:id=\"btnCalcola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
 
+        this.model = new Model();
+        this.boxMese.setItems(listaMesi);
+       
     }
 }
 
