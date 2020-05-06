16fede7cffa29a6b07593b2c956596f06aa9042b
arcan <arcan@DESKTOP-84JU088.homenet.telecomitalia.it>
2020-05-06 09:00:37 +0200

    Laboratorio 6 finito e funzionante

--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/EntryPoint.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/EntryPoint.java
@@ -2,6 +2,8 @@ package it.polito.tdp.meteo;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+
+import it.polito.tdp.meteo.model.Model;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -22,8 +24,8 @@ public class EntryPoint extends Application {
 
         controller = loader.getController();
     	
-        // TODO impostare il model nel controller        
-        
+        // TODO impostare il model nel controller 
+  
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
         stage.show();
